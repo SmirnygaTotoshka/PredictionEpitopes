91a4560290a6e155804cae0526f9b073599cf5a6
Anton Smirnov <anton.smirnov.9910@gmail.com>
2023-02-18 20:17:46 +0300

    reorganize projects, extract scripts from reports

---
+++ scripts/R/iedb_clean_mhc_bind.R
@@ -0,0 +1,120 @@
+#Используемые библиотеки
+library(RMySQL)
+library(dplyr)
+library(stringr)
+library(knitr)
+library(vroom)
+
+#Их версии
+print(paste("RMySQL",packageVersion("RMySQL")))
+print(paste("dplyr",packageVersion("dplyr")))
+print(paste("stringr",packageVersion("stringr")))
+print(paste("knitr",packageVersion("knitr")))
+print(paste("vroom",packageVersion("vroom")))
+
+setwd("/home/stotoshka/Documents/Epitops/PredictionEpitopes")
+
+# Подклюение БД и выгрузка данных
+con = RMySQL::dbConnect(RMySQL::MySQL(),
+                        dbname='iedb',
+                        host='localhost',
+                        port=3306,
+                        user='stotoshka',
+                        password='meowmeow')
+
+bind.assays = RMySQL::dbReadTable(con, "epi_mhc_bind")
+elution.assays = RMySQL::dbReadTable(con, "epi_mhc_elution")
+vroom_write(bind.assays,"data/source/iedb_bind_assays.tsv")
+vroom_write(elution.assays, "data/source/iedb_elution_assays.tsv")
+
+#Bind assays
+
+bind.assays = vroom("data/source/iedb_bind_assays.tsv",delim = "\t") %>% as.data.frame()
+str(bind.assays)
+
+# Критерии фильтрации:
+# 1. У каждой записи должен быть источник
+# 2. У каждой записи должен быть исход эксперимента
+# 3. Последовательности должны быть записаны однобуквенным кодом для аминокислот
+# 4. Эпитоп должен находиться в референсной последовательности
+# 5. Эпитоп должен быть полным. Это не должен быть участок антигенного сайта или частью большего эпитопа.
+# 6. Информация в комментариях не должна противоречить исходу эксперимента 
+# 7. Результаты должны быть получены экспериментально, не быть предсказанными
+# 8. Эпитопы должны быть иммуногенными
+# 9. Не должно быть дефицита по TAP
+# 10. Все HLA аллели должны быть записаны в формате, позволяющем однозначно идентифицировать молекулу
+
+#1
+sum(is.na(bind.assays$reference_id)) == 0
+#2
+sum(is.na(bind.assays$as_char_value)) == 0
+#3
+sum(grepl("[ACDEFGHIKLMNPQRSTVWYU]",bind.assays$linear_peptide_seq)) == nrow(bind.assays)
+sum(grepl("[ACDEFGHIKLMNPQRSTVWYU]",bind.assays$linear_peptide_seq)) == nrow(bind.assays)
+#4
+ind = seq_len(nrow(bind.assays))
+has.epitope = sapply(ind,function(i){
+  grepl(bind.assays$linear_peptide_seq[i],bind.assays$sequence[i],fixed = T)
+})
+sum(has.epitope) == nrow(bind.assays)
+#5
+kable(round(table(bind.assays$e_region_domain_flag) / nrow(bind.assays) * 100,2),col.names = c("Value","Procent"))
+#6
+print(paste("Количество",length(unique(bind.assays$as_comments))))
+print(paste("Количество записей",sum(!is.na(bind.assays$as_comments))))
+write.table(unique(bind.assays$as_comments),"data/bind_assay_comments.txt")
+kable(table(subset(bind.assays,grepl("did not",bind.assays$as_comments,fixed = T), select = "as_char_value")),caption = "did not")
+kable(table(subset(bind.assays,grepl("bad",bind.assays$as_comments,fixed = T), select = "as_char_value")),caption = "bad")
+#7
+kable(table(subset(bind.assays,grepl("was predicted",bind.assays$as_comments,fixed = T), select = "as_char_value")),caption = "was predicted")
+#8
+kable(table(subset(bind.assays,grepl("non-immunogenic",bind.assays$as_comments,fixed = T), select = "as_char_value")),caption = "non-immunogenic")
+#9
+kable(table(subset(bind.assays,grepl("TAP-deficient",bind.assays$as_comments,fixed = T), select = "as_char_value")),caption = "non-immunogenic")
+#10
+sum(grepl("^HLA-[ABC]\\*\\d{2}:\\d{2}$",bind.assays$chain_i_name)) == nrow(bind.assays)
+
+# Условия с 4 до 9 не соблюдаются. Исправим. Удалим все несовпадения, исправим противоречия, 
+# исправим as_char_value
+# Выделим результаты, полученные на TAP-дефицитном материале в отдельный датасет
+print(paste("Количество строк до",dim(bind.assays)[1]))
+
+bind.assays[grep("Positive", bind.assays$as_char_value),"as_char_value"] = "Positive"
+bind.assays = bind.assays[has.epitope,]
+bind.assays = bind.assays[grepl("Exact Epitope", bind.assays$e_region_domain_flag,fixed = T),]
+
+bind.assays[grepl("bad",bind.assays$as_comments,fixed = T) & bind.assays$as_char_value == "Positive","as_char_value"] = 'Negative'
+bind.assays[grepl("did not",bind.assays$as_comments,fixed = T) & bind.assays$as_char_value == "Positive","as_char_value"] = 'Negative'
+
+tap.def = bind.assays[grepl("TAP-deficient",bind.assays$as_comments,fixed = T),]
+vroom_write(tap.def,"data/tap_def_bind.csv")
+
+bind.assays = bind.assays[!grepl("TAP-deficient",bind.assays$as_comments,fixed = T),]
+bind.assays = bind.assays[!grepl("was predicted",bind.assays$as_comments,fixed = T),]
+bind.assays = bind.assays[!grepl("non-immunogenic",bind.assays$as_comments,fixed = T),]
+
+print(paste("Количество строк после",dim(bind.assays)[1]))
+
+#Сохраним и будем дальше с ним работать
+vroom_write(bind.assays, "data/source/iedb_bind_assays_clean.tsv")
+
+#----------------------------------------------------------------
+
+bind.assays = bind.assays[has.epitope,]
+print(paste("Количество строк после",dim(bind.assays)[1]))
+
+kable(table(bind.assays$as_char_value))
+
+bind.assays[grep("Positive", bind.assays$as_char_value),"as_char_value"] = "Positive"
+kable(table(bind.assays$as_char_value), col.names = c("Value","Freq"),format = "pipe")
+
+kable(round(table(bind.assays$category) / nrow(bind.assays) * 100,2),col.names = c("Value","Procent"))
+kable(round(table(bind.assays$assay_type) / nrow(bind.assays) * 100,2),col.names = c("Value","Procent"))
+round(sum(!is.na(bind.assays$as_num_value)) / nrow(bind.assays) * 100,2)
+eq.val = bind.assays[grepl("=",bind.assays$as_inequality,fixed = T),c("as_char_value","as_num_value","category","chain_i_name")]
+print(nrow(eq.val))
+kable(round(table(eq.val$as_char_value) / nrow(eq.val) * 100,2),col.names = c("Value","Procent"))
+
+length(unique(bind.assays$organism_name))
+length(unique(bind.assays$chain_i_name))
+sum(grepl("^HLA-[ABC]\\*\\d{2}:\\d{2}$",bind.assays$chain_i_name)) == nrow(bind.assays)
---
+++ scripts/sql/iedb_create_mhc_tables.sql
@@ -0,0 +1,100 @@
+#Скрипт iedb_create_mhc_tables.sql содержит описание первичной подготовки 
+#таблиц для модели связывания MHC-epitope.
+
+#Критерии фильтрации следующие:
+#  - mhc_bind и mhc_elution
+#    - Должен быть ИД публикации
+#    - Должен быть ИД эпитопа
+#    - Должен быть исход эксперимента
+#    - Должен быть ИД аллеля MHC
+#  - для эпитопов
+#    - Должна быть линейная последовательность эпитопа
+#    - Эта последовательность не должна содержать модификаций
+#    - Должна быть последовательность белка-источника.
+#  - для аллеля МНС
+#    - аллель должен быть человеческий
+#    - аллель должен быть первого класса МНС
+#    - аллель должен быть записан в формате HLA-<ген>*<аллельная группа>:<белковый продукт>
+
+CREATE TABLE filtered_bind
+AS (SELECT reference_id, curated_epitope_id, as_char_value,
+as_location, category, assay_type,units,
+as_num_value,as_inequality,as_comments, mhc_allele_restriction_id 
+    FROM mhc_bind
+    INNER JOIN assay_type ON as_type_id = assay_type_id
+    WHERE reference_id IS NOT NULL AND
+          curated_epitope_id IS NOT NULL AND
+          as_char_value IS NOT NULL AND
+          mhc_allele_restriction_id IS NOT NULL);
+          
+CREATE INDEX filtered_bind_index ON filtered_bind (curated_epitope_id,mhc_allele_restriction_id);
+
+CREATE TABLE filtered_elution
+AS (SELECT reference_id, curated_epitope_id, as_char_value, as_location, 
+category, assay_type, as_num_value, as_inequality, units, 
+as_num_subjects, as_num_responded, as_response_frequency, as_comments,
+as_immunization_comments,h_sex, h_age, mhc_allele_restriction_id,
+h_organism_id, ant_type, ant_ref_name, ant_object_id, apc_cell_type,
+apc_tissue_type, apc_origin
+    FROM mhc_elution
+    INNER JOIN assay_type ON as_type_id = assay_type_id
+    WHERE reference_id IS NOT NULL AND
+          curated_epitope_id IS NOT NULL AND
+          as_char_value IS NOT NULL AND
+          mhc_allele_restriction_id IS NOT NULL);
+          
+CREATE INDEX filtered_elution_index ON filtered_elution (curated_epitope_id,mhc_allele_restriction_id);
+
+CREATE TABLE filtered_epitope_cur
+AS (SELECT curated_epitope_id, e_name, 
+source_antigen_accession, description ,e_region_domain_flag,
+e_ev, linear_peptide_seq, e_ref_start,
+e_ref_end, `database`, name, sequence, organism_name
+    FROM curated_epitope
+    INNER JOIN epitope_object eo ON eo.object_id = e_object_id
+    INNER JOIN epitope e ON e.epitope_id = eo.epitope_id
+    INNER JOIN source s ON s.accession = source_antigen_accession
+    WHERE linear_peptide_seq IS NOT NULL AND 
+          linear_peptide_modification IS NULL AND
+          sequence IS NOT NULL);
+          
+CREATE INDEX filtered_epitope_cur_index ON filtered_epitope_cur (curated_epitope_id);
+
+#9606 - Taxonomy ID для Homo Sapiens
+CREATE TABLE filtered_mhc
+AS (SELECT mhc_allele_restriction_id, 
+restriction_level,displayed_restriction, organism_ncbi_tax_id,class, chain_i_name
+    FROM mhc_allele_restriction
+    WHERE restriction_level = 'complete molecule' AND
+          organism_ncbi_tax_id = 9606 AND
+          class = 'I');
+          
+CREATE INDEX filtered_mhc_index ON filtered_mhc (mhc_allele_restriction_id);
+
+
+CREATE TABLE epi_mhc_bind
+AS (SELECT fb.curated_epitope_id, fb.mhc_allele_restriction_id, reference_id, as_char_value, as_location, 
+category, assay_type,units,as_num_value,as_inequality,as_comments,
+e_name, source_antigen_accession, description ,e_region_domain_flag, e_ev, linear_peptide_seq, 
+e_ref_start,e_ref_end, `database`, name, sequence, organism_name, 
+restriction_level,displayed_restriction, organism_ncbi_tax_id,class, chain_i_name
+    FROM filtered_bind fb
+    INNER JOIN filtered_mhc fm ON fm.mhc_allele_restriction_id = fb.mhc_allele_restriction_id
+    INNER JOIN filtered_epitope_cur fe ON fe.curated_epitope_id = fb.curated_epitope_id);
+    
+CREATE INDEX epi_mhc_bind_index ON epi_mhc_bind (curated_epitope_id,mhc_allele_restriction_id);
+
+CREATE TABLE epi_mhc_elution
+AS (SELECT reference_id, fel.curated_epitope_id, as_char_value, as_location, 
+category, assay_type, as_num_value, as_inequality, units, 
+as_num_subjects, as_num_responded, as_response_frequency, as_comments,
+as_immunization_comments,h_sex, h_age, fel.mhc_allele_restriction_id,
+h_organism_id, ant_type, ant_ref_name, ant_object_id, apc_cell_type,
+apc_tissue_type, apc_origin,
+e_name, source_antigen_accession, description ,e_region_domain_flag, e_ev, linear_peptide_seq, e_ref_start,e_ref_end, `database`, name, sequence, organism_name, 
+restriction_level,displayed_restriction, organism_ncbi_tax_id,class, chain_i_name
+    FROM filtered_elution fel
+    INNER JOIN filtered_mhc fm ON fm.mhc_allele_restriction_id = fel.mhc_allele_restriction_id
+    INNER JOIN filtered_epitope_cur fe ON fe.curated_epitope_id = fel.curated_epitope_id);
+    
+CREATE INDEX epi_mhc_elution_index ON epi_mhc_elution (curated_epitope_id,mhc_allele_restriction_id);
